5bbd89c716f911cc9aaab8b8a2c895bd640a66b4
imamuratakuma <imamuratakumagit@gmail.com>
2023-04-20 09:51:34 +0900

    App01 commit reset機能を追加

--- app/src/main/java/com/example/app01/MainActivity.java
+++ app/src/main/java/com/example/app01/MainActivity.java
@@ -11,8 +11,11 @@ public class MainActivity extends AppCompatActivity {   // アプリ本体であ
 
     private TextView text1;     // TextViewの変数 text1 を用意
     private TextView text2;     // TextViewの変数 text2 を用意
+    private TextView text3;
     private Button btn1;        // Buttonの変数 btn1 を用意
     private Button btn2;        // Buttonの変数 btn2 を用意
+    private Button btn3;
+    private Button Reset;
 
     @Override   // オーバーライドアノテーション（このメソッドは親からオーバーライドしたものだと表す）
     protected void onCreate(Bundle savedInstanceState) {    // アプリが起動したときに呼ばれるメソッド
@@ -21,22 +24,38 @@ public class MainActivity extends AppCompatActivity {   // アプリ本体であ
 
         text1 = findViewById(R.id.text1);                   // text1というIDのViewを取得しtext1変数に代入
         text2 = findViewById(R.id.text2);                   // text2というIDのViewを取得しtext2変数に代入
+        text3 = findViewById(R.id.text3);
         btn1 = findViewById(R.id.btn1);                     // btn1というIDのViewを取得しbtn1変数に代入
         btn2 = findViewById(R.id.btn2);                     // btn2というIDのViewを取得しbtn2変数に代入
+        btn3 = findViewById(R.id.btn3);                     // btn2というIDのViewを取得しbtn2変数に代入
+        Reset = findViewById(R.id.reset);
 
-        btn1.setOnClickListener(new View.OnClickListener() {// btn1にクリックリスナーを設定（定義も同時に行っている）
+        btn1.setOnClickListener(new View.OnClickListener() {   // btn1にクリックリスナーを設定（定義も同時に行っている）
             @Override
-            public void onClick(View view) {	// 設定したViewがクリックされたら実行されるメソッド
-                text1.setText("こんにちは");     // text1のテキストを変更
-                text2.setText("初めまして");     // text2のテキストを変更
+            public void onClick(View view) {	               // 設定したViewがクリックされたら実行されるメソッド
+                text1.setText("私");                           // text1のテキストを変更
+
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener() {// btn2にクリックリスナーを設定（定義も同時に行っている）
+        btn2.setOnClickListener(new View.OnClickListener() {    // btn2にクリックリスナーを設定（定義も同時に行っている）
             @Override
-            public void onClick(View view) {		// 設定したViewがクリックされたら実行されるメソッド
-                text1.setText("こんばんは");             // text1のテキストを変更
-                text2.setText("よろしくお願いします");   // text2のテキストを変更
+            public void onClick(View view) {		            // 設定したViewがクリックされたら実行されるメソッド
+                text2.setText("好き");                          // text1のテキストを変更
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {    // btn3にクリックリスナーを設定（定義も同時に行っている）
+            @Override
+            public void onClick(View view) {		            // 設定したViewがクリックされたら実行されるメソッド
+                text3.setText("バスケットボール");              // text3のテキストを変更
+            }
+        });
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text1.setText("I");
+                text2.setText("like");
+                text3.setText("basketball");
             }
         });
 
